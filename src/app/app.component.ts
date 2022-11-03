43fbd5181142d294c714f3a40198b7f5f8d95f12
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-11-04 00:38:08 +0530

    Added: The Validation to the Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld';
-  imgUrl="../assets/download.jpg";
+  title = 'HelloWorldBridgelabz';
+  imgUrl = "../assets/download.jpg";
   url = "https://www.bridgelabz.com";
-  userName: string = "";
+  userName: string="";
+  nameError: string= "";
 
-  ngOnInit():void { 
-    this.title = "Hello from Bridgelabz";
-}
-
-onClick($event: any){
-  console.log("Save button is clicked!", $event);
-  window.open(this.url, "_blank");
-}
-
+  ngOnInit(): void{
+    this.title="Hello From Bridgelabz";
+  }
+  
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+  
+  onInput($event:any){
+    console.log("change Event Occured", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
+  }
 }
